adc887940801e5d3cf841e7a1ddf4ce6509befbd
Ranadheer <radr314@gmail.com>
2025-02-02 14:47:27 -0500

    save model

--- models/training/reg_model.py
+++ models/training/reg_model.py
@@ -3,14 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 import os
+import joblib
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-path=Path(r"C:\Users\ranad\Desktop\noline\data\Backpack-Prediction-Challenge")
+import time
+path=Path(r"/mnt/c/Users/ranad/Desktop/noline/data/Backpack-Prediction-Challenge")
 
 data_pd=pd.read_csv(path/"train.csv")
-# data_pd
-
 
 data_pd=data_pd.rename(columns={'Weight Capacity (kg)':'Weight-Capacity'})
 modified_columns=list(map(lambda x:x.replace(' ','-').lower(),data_pd.columns))
@@ -26,7 +26,13 @@ data_pd=pd.get_dummies(data_pd, columns=list(og_categorical_columns), drop_first
 X=data_pd.drop(columns=['price'])
 y=data_pd['price']
 
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model = RandomForestRegressor(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
 
+t1=time.time()
+model.fit(X_train, y_train)
+t2=time.time()
+print("time taken : ",t2-t1)
+
+joblib.dump(model, "random_forest_v1.pkl")
