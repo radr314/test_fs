329b801c6a126a026b539d71a5ff2a9b79fc635a
radr314 <radr314@gmail.com>
2025-03-22 23:50:36 -0400

    flask

--- flask/flaskblog.py
+++ flask/flaskblog.py
@@ -21,7 +21,7 @@ posts=[
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html',posts=posts)
+    return render_template('home.html',posts=posts, title='Test')
 
 
 @app.route("/about")
